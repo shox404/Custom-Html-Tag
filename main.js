a2b4084752be226a6a887812b10638a234242b64
Shoxruh <shxxx404@gmail.com>
2024-08-18 20:54:38 +0500

    Update

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-
 class Counter extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-      this.render();
-    }
-  
-    connectedCallback() {
-      this.render();
-    }
-  
-    minus() {
-      let value = parseInt(this.getAttribute("value"), 10) || 0;
-      this.setAttribute("value", value - 1);
-      this.render();
-    }
-  
-    plus() {
-      let value = parseInt(this.getAttribute("value"), 10) || 0;
-      this.setAttribute("value", value + 1);
-      this.render();
-    }
-  
-    render() {
-      this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.render();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  minus() {
+    let value = parseInt(this.getAttribute("value"), 10) || 0;
+    this.setAttribute("value", value - 1);
+    this.render();
+  }
+
+  plus() {
+    let value = parseInt(this.getAttribute("value"), 10) || 0;
+    this.setAttribute("value", value + 1);
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
         <style>
           button {
             font-size: 20px;
@@ -36,12 +35,15 @@ class Counter extends HTMLElement {
           <button id="plus">+</button>
         </div>
       `;
-  
-      // Attach event listeners
-      this.shadowRoot.getElementById('minus').addEventListener('click', () => this.minus());
-      this.shadowRoot.getElementById('plus').addEventListener('click', () => this.plus());
-    }
+
+    // Attach event listeners
+    this.shadowRoot
+      .getElementById("minus")
+      .addEventListener("click", () => this.minus());
+    this.shadowRoot
+      .getElementById("plus")
+      .addEventListener("click", () => this.plus());
   }
-  
-  customElements.define("custom-counter", Counter);
-  
+}
+
+customElements.define("custom-counter", Counter);
