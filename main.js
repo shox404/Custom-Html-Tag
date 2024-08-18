ada214ed235f351b5386cb21110351e082ebb2cd
Shoxruh <shxxx404@gmail.com>
2024-08-18 20:54:08 +0500

    Update

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
+
 class Counter extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.render();
+    }
+  
+    connectedCallback() {
+      this.render();
+    }
+  
+    minus() {
+      let value = parseInt(this.getAttribute("value"), 10) || 0;
+      this.setAttribute("value", value - 1);
+      this.render();
+    }
+  
+    plus() {
+      let value = parseInt(this.getAttribute("value"), 10) || 0;
+      this.setAttribute("value", value + 1);
+      this.render();
+    }
+  
+    render() {
+      this.shadowRoot.innerHTML = `
+        <style>
+          button {
+            font-size: 20px;
+            margin: 5px;
+          }
+        </style>
+        <div>
+          <button id="minus">-</button>
+          ${this.getAttribute("value") || 0}
+          <button id="plus">+</button>
+        </div>
+      `;
+  
+      // Attach event listeners
+      this.shadowRoot.getElementById('minus').addEventListener('click', () => this.minus());
+      this.shadowRoot.getElementById('plus').addEventListener('click', () => this.plus());
+    }
   }
-
-  connectCallback() {
-    this.render();
-  }
-
-  render() {
-    this.shadowRoot.innerHTML = `
-     <style>
-        div {
-          background: #e1e1e1;
-        }
-     </style>
-     <div><slot></slot></div> 
-    `;
-  }
-}
-
-customElements.define("custom-counter", Counter);
+  
+  customElements.define("custom-counter", Counter);
+  
